8cb6e353149eb383cb0026de94617f023391a619
myselfregmi <pushparegmi0@gmail.com>
2023-12-27 12:55:46 +0545

    First Phase

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Movie from "./Pages/Movie";
+
+const App = () => {
+  return (
+    <div>
+      <Movie />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/Pages/Movie.jsx
@@ -0,0 +1,43 @@
+import React, { useEffect, useState } from "react";
+
+const Movie = () => {
+  const [data, setData] = useState([]);
+  try {
+    const fetchData = async () => {
+      const response = await fetch(
+        "https://api.themoviedb.org/3/discover/movie?api_key=23e9efd7fcc25acf48d0ae2cab434317"
+      );
+      const value = await response.json();
+      setData(value.results);
+      console.log(data);
+    };
+    useEffect(() => {
+      fetchData();
+    }, [setData]);
+  } catch (error) {
+    console.log("Error", error);
+  }
+  return (
+    <div>
+      <div>
+        Movie
+        <div>
+          {data.map((items) => {
+            return (
+              <div key={items.id}>
+                <div>{items.title}</div>
+                <div>
+                  <img
+                    src={`https://image.tmdb.org/t/p/w500${items.poster_path}`}
+                  />
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Movie;
